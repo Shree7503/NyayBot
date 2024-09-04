3deb09edf3a22c193be6703d94e36b580ae2c545
Shreenath Kadam <146519199+Shree7503@users.noreply.github.com>
2024-09-04 10:28:48 +0530

    --v

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import path from "path";
 
-const app: Express = express();
+const app: Express = express(); 
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -18,3 +18,5 @@ app.get("/info", (req: Request, res: Response) => {
 app.listen(8000, () => {
     console.log("Server Running at PORT: 8000");
 });
+
+export default app;
