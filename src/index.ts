a012a2d5596b6b51a8e30ed611f170218a6a50b5
Shreenath Kadam <146519199+Shree7503@users.noreply.github.com>
2024-09-16 19:33:39 +0530

    changed card name

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,22 @@ import express, { Express, Request, Response } from "express";
 import path from "path";
 import data from "./info";
 
-const app: Express = express(); 
+const app: Express = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req: Request, res: Response) => {
-    res.render("index");
+  res.render("index");
 });
 
 app.get("/info", (req: Request, res: Response) => {
-    res.render("info", { data: data });
+  res.render("info", { data: data });
 });
 
 app.listen(8000, () => {
-    console.log("Server Running at PORT: 8000");
+  console.log("Server Running at PORT: 8000");
 });
 
 export default app;
